7237bae87916854cc6d411ebfc172ca7b222a3b1
“shlokster” <bhosaleshlok@gmail.com>
2021-05-21 15:00:41 +0530

    Second Commit

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.first_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b1;
+    private EditText et1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
